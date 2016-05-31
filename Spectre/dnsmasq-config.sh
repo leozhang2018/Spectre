5d6bd7342deff926edbfcb0550a6eb9cdb5d755d
leozhang2018 <leozhang2018@gmail.com>
2016-05-31 11:33:10 +0800

    Update script

--- Spectre/dnsmasq-config.sh
+++ Spectre/dnsmasq-config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # dnsmasq 导入配置脚本
 
-
 # 载入 dnsmasq 配置函数
 dnsmasqImportConf(){
 
@@ -14,13 +13,23 @@ dnsmasqCheck(){
 
 if which dnsmasq > /dev/null;then
 	echo -e "Dnsmasq exists \n"
-	#载入 dnsmasq 配置
-	dnsmasqImportConf
+      #检测 dnsmasq 是否运行
+	if `grep | service dnsmasq status`=="" ;then
+      #若运行则停止服务
+      sudo service dnsmasq stop
+      #载入 dnsmasq 配置
+    	dnsmasqImportConf
+      else
+      #载入 dnsmasq 配置
+      dnsmasqImportConf
+  fi
+
 	else
 	echo -e "Dnsmasq not exists \n"
+  #安装 dnsmasq
 	sudo apt-get update && sudo apt-get install dnsmasq
 	#载入 dnsmasq 配置
-        dnsmasqImportConf
+  dnsmasqImportConf
 fi
 
 }
