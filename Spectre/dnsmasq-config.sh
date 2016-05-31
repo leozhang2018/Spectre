d23a0d73fb9676b97bd435576feb69521330470c
leozhang2018 <leozhang2018@gmail.com>
2016-05-31 15:41:27 +0800

    Add dnsmasq.d import

--- Spectre/dnsmasq-config.sh
+++ Spectre/dnsmasq-config.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # dnsmasq 导入配置脚本
 
-# 载入 dnsmasq 配置函数
-dnsmasqImportConf(){
+# 载入 Spectre 配置文件
+. /Spectre/config.conf
 
-	# 载入 dnsmasq 配置
-        . /Spectre/dnsmasq.conf.sh >> /etc/dnsmasq2.conf
+# 载入 dnsmasq 配置函数
+function dnsmasqImportConf(){
+
+	  # 载入 dnsmasq 配置
+    . /Spectre/dnsmasq.conf.sh >> /etc/dnsmasq2.conf
 
 }
 
-dnsmasqCheck(){
+function dnsmasqCheck(){
 
 if which dnsmasq > /dev/null;then
+  # 如果已经安装 dnsmasq
 	echo -e "Dnsmasq exists \n"
       #检测 dnsmasq 是否运行
-	if `grep | service dnsmasq status`=="" ;then
+	if  service dnsmasq status|grep "Active: active" >> /dev/null ;then #Bug
       #若运行则停止服务
       sudo service dnsmasq stop
       #载入 dnsmasq 配置
@@ -25,16 +29,29 @@ if which dnsmasq > /dev/null;then
   fi
 
 	else
-	echo -e "Dnsmasq not exists \n"
-  #安装 dnsmasq
-	sudo apt-get update && sudo apt-get install dnsmasq
-	#载入 dnsmasq 配置
-  dnsmasqImportConf
+	   echo -e "Dnsmasq not install \n"
+     #安装 dnsmasq
+	   sudo apt-get update && sudo apt-get install dnsmasq
+	   #载入 dnsmasq 配置
+     dnsmasqImportConf
 fi
 
 }
+
+#复制已有的配置文件至 /etc/dnsmasq.d
+function importDnsmasqd(){
+
+#复制文件
+sudo cp -R /Spectre/dnsmasq.d /etc
+
+}
+
+
 # 运行 dnsmasqCheck 函数
 dnsmasqCheck
+
+# 运行复制配置函数 importDnsmasqd
+importDnsmasqd
 
 # 重启dnsmasq 服务
 sudo service dnsmasq restart
